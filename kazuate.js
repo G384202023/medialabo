86dc4fa0d848c9568728d3a9acca8ba69ee8cc05
G384202023 <238420@st.takushoku-u.ac.jp>
2024-06-20 16:45:53 +0900

    こう？

--- kazuate.js
+++ kazuate.js
@@ -19,21 +19,20 @@ function hantei() {
   let yoso = 4;
   let kotae = Math.floor(Math.random()*10);
 
-  for (let n of kaisu) {
-    kaisu++;
-    console.log(kaisu + '回目の予想: 4');
+  
+  console.log(kaisu + '回目の予想: 4');
     if(kaisu<=4){
         console.log('答えは ' + kotae + ' でした．すでにゲームは終わっています');
-      }else if(yoso===4){
+    }else if(yoso===4){
         console.log('正解です．おめでとう!');
-      }else if(kaisu===3){
+    }else if(kaisu===3){
         console.log('まちがい．残念でした答えは' + kotae + 'です．');
-      }else if(kaisu<=2 && yoso<kotae){
+    }else if(kaisu<=2 && yoso<kotae){
         console.log('まちがい．答えはもっと大きいですよ');
-      }else if(kaisu<=2 && yoso>kotae){
+    }else if(kaisu<=2 && yoso>kotae){
         console.log('まちがい．答えはもっと小さいですよ');
     }
-  }
+  kaisu++;
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
