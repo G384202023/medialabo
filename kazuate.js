9dbfc124487e05611f9c288f6448aeb7c8a42065
G384202023 <238420@st.takushoku-u.ac.jp>
2024-06-27 17:25:43 +0900

    課題４

--- kazuate.js
+++ kazuate.js
@@ -1,12 +1,15 @@
-// 答え
+// ///////////////答え
 let kotae = Math.floor(Math.random()*10) + 1;
-console.log('答え（デバッグ用）: ' + kotae);
+//console.log('答え（デバッグ用）: ' + kotae);
 
-// 入力回数（予想回数）
-let kaisu = 1;
+///////////////////////入力回数（予想回数）
+let kaisu = 4;
 // 予想を4回実行する
-// 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+
+/////////////////////将来以下の hantei(); の4回の呼び出しを全て削除する
+
+
+///////////////////////////代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 hantei();
 hantei();
 hantei();
@@ -18,7 +21,11 @@ function hantei() {
   let yoso = 4;
 
   
-  console.log(kaisu + '回目の予想: 4');
+  //console.log(kaisu + '回目の予想: 4');
+  let maru = document.querySelector('span#kaisu');
+  maru.textContent = kaisu;
+  let squad = document.querySelector('span#answer');
+  squad.textContent = yoso;
     if(kaisu>=4){
         console.log('答えは ' + kotae + ' でした．すでにゲームは終わっています');
     }else if(kotae===4){
