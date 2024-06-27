809d75cd6a7a4d582f7eba1999a9b5ccfa968673
G384202023 <238420@st.takushoku-u.ac.jp>
2024-06-27 17:04:20 +0900

    4-5aft

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -58,5 +58,5 @@ let itiran = document.querySelector('ul#location');
 for(let n of data){
 	let li = document.createElement('li');
 	li = n.name + " ... 緯度:" + n.lat + ", 経度:" + n.lng;
-	itiran.insertAdjacentElement('beforeend', li);
+	itiran.insertAdjacentElement('afterend', li);
 }
